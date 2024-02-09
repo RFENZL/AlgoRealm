53f57b7eb0f7900b164c89eaae1dda68fcc8557b
GaetanccgYnov <gaetan.chollet@ynov.com>
2024-02-09 09:31:02 +0100

    Archi + main pages

--- Views/Medias/script.js
+++ Views/Medias/script.js
@@ -1,14 +1,11 @@
 document.getElementById('start-button').addEventListener('click', function() {
-    // Commence le jeu ou redirige vers la page du jeu
-    window.location.href = 'game.html'; // Remplacez avec l'URL de votre jeu
+    window.location.href = 'game.html';
 });
 
 document.getElementById('options-button').addEventListener('click', function() {
-    // Ouvre les options du jeu
-    alert('Options pas encore implémentées!');
+    window.location.href = 'Options.html';
 });
 
 document.getElementById('credits-button').addEventListener('click', function() {
-    // Affiche les crédits du jeu
     alert('Crédits pas encore implémentés!');
 });
