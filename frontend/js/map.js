fd086ab8c80de9e364d310665e1fdd909f7786dc
GaetanccgYnov <gaetan.chollet@ynov.com>
2024-05-24 10:08:34 +0200

    Add default map dans mouvements

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,11 +1,13 @@
-var config = {
+const config = {
     type: Phaser.AUTO,
-    width: window.innerWidth,
-    height: window.innerHeight,
+    width: 800,
+    height: 600,
+    parent: "game-container",
+    pixelArt: true,
     physics: {
-        default: 'arcade',
+        default: "arcade",
         arcade: {
-            gravity: { y: 200 }
+            gravity: { y: 0 }
         }
     },
     scene: {
@@ -15,69 +17,120 @@ var config = {
     }
 };
 
-var game = new Phaser.Game(config);
-var character;
+const game = new Phaser.Game(config);
+let cursors;
+let player;
+let showDebug = false;
 
-function preload ()
-{
-    // Chargez le fichier JSON de la carte Tiled
-    this.load.tilemapTiledJSON('map', 'assets/maps_json/map.json');
+function preload() {
+    this.load.image("tiles", "assets/tilesets/tuxmon-sample-32px-extruded.png");
+    this.load.tilemapTiledJSON("map", "assets/tilemaps/tuxemon-town.json");
 
-    // Chargez les images de la carte
-    this.load.image('sol', 'assets/sol.png');
-    this.load.image('arbres', 'assets/arbres.png');
-    this.load.image('houses', 'assets/houses.png');
+    this.load.atlas("atlas", "assets/atlas/atlas.png", "assets/atlas/atlas.json");
+}
 
-    // Chargez l'image du personnage
-    this.load.image('character', 'assets/main_menu/gemme_logo.png');
+function create() {
+    const map = this.make.tilemap({ key: "map" });
+    const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
 
-    this.load.on('complete', () => {
-        // Créez un personnage à partir de la classe Character
-        character = new Character(this, 400, 300, 'character');
+    const belowLayer = map.createLayer("Below Player", tileset, 0, 0);
+    const worldLayer = map.createLayer("World", tileset, 0, 0);
+    const aboveLayer = map.createLayer("Above Player", tileset, 0, 0);
+
+    worldLayer.setCollisionByProperty({ collides: true });
+    aboveLayer.setDepth(10);
+
+    const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
+
+    player = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
+        .setSize(30, 40)
+        .setOffset(0, 24);
+
+    this.physics.add.collider(player, worldLayer);
+
+    const anims = this.anims;
+    anims.create({
+        key: "misa-left-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    anims.create({
+        key: "misa-right-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    anims.create({
+        key: "misa-front-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+    anims.create({
+        key: "misa-back-walk",
+        frames: anims.generateFrameNames("atlas", { prefix: "misa-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    const camera = this.cameras.main;
+    camera.startFollow(player);
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+
+    cursors = this.input.keyboard.createCursorKeys();
+
+    this.add.text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
+        font: "18px monospace",
+        fill: "#000000",
+        padding: { x: 20, y: 10 },
+        backgroundColor: "#ffffff"
+    }).setScrollFactor(0).setDepth(30);
+
+    this.input.keyboard.once("keydown-D", event => {
+        this.physics.world.createDebugGraphic();
+        const graphics = this.add.graphics().setAlpha(0.75).setDepth(20);
+        worldLayer.renderDebug(graphics, {
+            tileColor: null,
+            collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255),
+            faceColor: new Phaser.Display.Color(40, 39, 37, 255)
+        });
     });
 }
 
-function create ()
-{
-    // Create the map from the Tiled JSON file
-    var map = this.make.tilemap({ key: 'map' });
+function update(time, delta) {
+    const speed = 300;
+    const prevVelocity = player.body.velocity.clone();
 
-    // Add the tileset images
-    var solTiles = map.addTilesetImage('sol');
-    var arbresTiles = map.addTilesetImage('arbres');
-    var housesTiles = map.addTilesetImage('houses');
+    player.body.setVelocity(0);
 
-    // Create the map layers
-    var solLayer = map.createLayer('sol', solTiles);
-    var arbresLayer = map.createLayer('arbres', arbresTiles);
-    var housesLayer = map.createLayer('houses', housesTiles);
-
-    // Set the collisions for the map layers
-    solLayer.setCollisionByProperty({ collides: true });
-    arbresLayer.setCollisionByProperty({ collides: true });
-    housesLayer.setCollisionByProperty({ collides: true });
-
-    // Add the collision between the character and the map layers
-    this.physics.add.collider(character.sprite, solLayer);
-    this.physics.add.collider(character.sprite, arbresLayer);
-    this.physics.add.collider(character.sprite, housesLayer);
-}
-
-function update ()
-{
-    character.stop();
-
-    if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT), 1)) {
-        character.moveLeft(200);
-    }
-    else if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT), 1)) {
-        character.moveRight(200);
+    if (cursors.left.isDown) {
+        player.body.setVelocityX(-speed);
+    } else if (cursors.right.isDown) {
+        player.body.setVelocityX(speed);
     }
 
-    if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP), 1)) {
-        character.moveUp(200);
+    if (cursors.up.isDown) {
+        player.body.setVelocityY(-speed);
+    } else if (cursors.down.isDown) {
+        player.body.setVelocityY(speed);
     }
-    else if (this.input.keyboard.checkDown(this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN), 1)) {
-        character.moveDown(200);
+
+    player.body.velocity.normalize().scale(speed);
+
+    if (cursors.left.isDown) {
+        player.anims.play("misa-left-walk", true);
+    } else if (cursors.right.isDown) {
+        player.anims.play("misa-right-walk", true);
+    } else if (cursors.up.isDown) {
+        player.anims.play("misa-back-walk", true);
+    } else if (cursors.down.isDown) {
+        player.anims.play("misa-front-walk", true);
+    } else {
+        player.anims.stop();
+        if (prevVelocity.x < 0) player.setTexture("atlas", "misa-left-walk");
+        else if (prevVelocity.x > 0) player.setTexture("atlas", "misa-right-walk");
+        else if (prevVelocity.y < 0) player.setTexture("atlas", "misa-back-walk");
+        else if (prevVelocity.y > 0) player.setTexture("atlas", "misa-front-walk");
     }
 }
